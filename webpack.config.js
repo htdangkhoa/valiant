64b7425d67401567c8dcc44768f3139654cbc1d8
Huynh Tran Dang Khoa <huynhtran.dangkhoa@gmail.com>
2021-05-17 12:33:24 +0700

    improve logger

--- webpack.config.js
+++ webpack.config.js
@@ -43,7 +43,11 @@ module.exports = {
             }
           }
 
-          electronProcess = spawn('npm', ['start']);
+          electronProcess = spawn('npm', ['start'], {
+            shell: true,
+            env: process.env,
+            stdio: 'inherit',
+          });
         });
       },
     },
