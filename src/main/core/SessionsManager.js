be594de5996eefa4abb496ab5fbb766b2b25103b
Huynh Tran Dang Khoa <huynhtran.dangkhoa@gmail.com>
2021-06-10 01:01:22 +0700

    add simple request permission

--- src/main/core/SessionsManager.js
+++ src/main/core/SessionsManager.js
@@ -1,4 +1,5 @@
-import { session } from 'electron';
+import { session, BrowserWindow, dialog } from 'electron';
+import logger from 'common/logger';
 
 class SessionManager {
   constructor() {
@@ -17,6 +18,24 @@ class SessionManager {
         'serviceworkers',
         'cachestorage',
       ],
+    });
+
+    this.view.setPermissionRequestHandler((webContents, permission, callback, details) => {
+      logger.log(webContents.id, permission);
+
+      const win = BrowserWindow.getFocusedWindow();
+
+      if (!win) return;
+      const url = new URL(details.requestingUrl);
+
+      const id = dialog.showMessageBoxSync(win, {
+        type: 'question',
+        message: `${url.hostname} wants to:`,
+        detail: permission,
+        buttons: ['Allow', 'Block'],
+      });
+
+      callback(id === 0);
     });
   }
 }
